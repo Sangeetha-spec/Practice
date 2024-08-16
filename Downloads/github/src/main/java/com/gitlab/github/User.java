212805e2c5197f51684e86a87579a47f81e113b3
jaladi4ms <jaladi4ms@gmail.com>
2024-08-16 12:08:46 -0500

    testing

---
+++ Downloads/github/src/main/java/com/gitlab/github/User.java
@@ -0,0 +1,38 @@
+package com.gitlab.github;
+
+public class User {
+	
+	
+	public int id;
+	public String name;
+	public int employeedId;
+	public User(int id, String name, int employeedId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.employeedId = employeedId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEmployeedId() {
+		return employeedId;
+	}
+	public void setEmployeedId(int employeedId) {
+		this.employeedId = employeedId;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", employeedId=" + employeedId + "]";
+	}
+
+}
