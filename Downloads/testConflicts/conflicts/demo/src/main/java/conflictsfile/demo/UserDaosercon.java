d827646179f556d8de1807d09d1a68cff7ea4063
jaladi4ms <jaladi4ms@gmail.com>
2024-08-22 18:32:35 -0500

    hgfy

--- Downloads/testConflicts/conflicts/demo/src/main/java/conflictsfile/demo/UserDaosercon.java
+++ Downloads/testConflicts/conflicts/demo/src/main/java/conflictsfile/demo/UserDaosercon.java
@@ -8,7 +8,7 @@ public class UserDaosercon {
 public static List<Userconf> users = new ArrayList<>();
 	
 	static {
-		 users.add(new Userconf(1 ,"jjj", 2));
+		 users.add(new Userconf(1 ,"jjj", 12));
 		 users.add(new Userconf(2, "ddd",22));
 	}
 	public List<Userconf> getUser()
