41e784dd2f1c97021b574d0486a10353351ada8f
jaladi4ms <jaladi4ms@gmail.com>
2024-08-22 14:04:24 -0500

    gfuy

--- Downloads/github/src/main/java/com/gitlab/github/UserDaoService.java
+++ Downloads/github/src/main/java/com/gitlab/github/UserDaoService.java
@@ -7,7 +7,7 @@ public class UserDaoService {
 	public static List<User> users = new ArrayList<>();
 	
 	static {
-		    users.add(new User(1 ,"jjj", 999));
+		    users.add(new User(1 ,"jjj", 99));
 		    users.add(new User(2, "ddd",22));
 	}
 	public List<User> getUser()
